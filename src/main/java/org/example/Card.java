9bad33c0fbfed7d1901d99dd5ad94e2cc29caea8
nikko <nikkorivas@outlook.com>
2022-08-18 11:28:32 +0100

    created Card class

---
+++ src/main/java/org/example/Card.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Card {
+//    String[] suits = {"♥","♧","♦","♤"};
+//    String[] cards = {2,3,4,5,6,7,8,9,10,J,Q,K,A};
+    private String cardSuit;
+    private String cardFace;
+    private String cardValue;
+
+    public String getCardFace() {
+        return cardFace;
+    }
+    public String getCardSuit() {
+        return cardSuit;
+    }
+    public String getCardValue() {
+        return cardValue;
+    }
+}
+// each card
---
+++ src/main/java/org/example/Deck.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Deck {
+//♤ ♡ ♢ ♧
+//♠ ♥ ♦ ♣
+//♥ ♧ ♦ ♤
+
+    //String[] suits = {"♥","♧","♦","♤"}
+    private static final String Ac = "A♣";
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++){
+
+            for (int i2 = 0; i2 < 52; i2++) {
+                Card card = new Card()
+            }
+
+        }
+    }
+}
